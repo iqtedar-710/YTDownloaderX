914a55a935110dcbac2fb62200f9695d0bddd693
iqtedar-710 <iqtedarahmed70@gmail.com>
2026-05-19 13:39:46 +0500

    fixed cors issue

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,15 @@ import uuid
 import requests
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # ------------------------------------------------
 # CORS
 # ------------------------------------------------
